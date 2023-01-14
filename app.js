2ceb8bee2f61f8a6c5d42ac919cf4c40a415c47e
duanzin <lucaduan@gmail.com>
2023-01-13 21:30:48 -0300

    codigos não testados

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+try {
+  await mongoClient.connect();
+  console.log("MongoDB Connected!");
+} catch (err) {
+  console.log(err.message);
+}
+
+const db = mongoClient.db();
 
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
-    
-    res.send('Hello World');
+app.post("/participants", async (req, res) => {
+  const nome = req.body;
+  res.sendStatus(201);
+});
+
+app.get("/participants", (req, res) => {
+  const participantes = db.collection("participants").find().toArray();
+  res.send(participantes);
+});
+
+app.post("/messages", async (req, res) => {
+  const mensagem = req.body;
+  res.sendStatus(201);
+});
+
+app.get("/messages", (req, res) => {
+  const user = req.headers.user;
+  if (!req.query.limit) {
+    const mensagens = db.collection("messages").find().toArray();
+    res.send(mensagens);
+    return;
+  }
+  const limit = req.query.limit;
+  const mensagens = db.collection("messages").find().toArray();
+  res.send(mensagens);
+});
+
+app.post("/status", async (req, res) => {
+  const user = req.body;
+  res.sendStatus(200);
 });
 
 app.listen(5000);
